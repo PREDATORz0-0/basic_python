442183edb0073956662ee391d95a13b51d7850ca
Sibi <158076463+PREDATORz0-0@users.noreply.github.com>
2025-03-12 17:15:51 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -10,3 +10,11 @@ If the array contains the numbers: [1, 2, 3, 5, 6]
 Then the missing number is 4.
 
 """
+import random
+array = [5, 8, 12, 3, 6]
+f = random.randint(0, len(array) - 1)
+n = [x for i, x in enumerate(array) if i != f]
+print("Original array: ",array)
+print("Element to exclude: ",array[f])
+print("Array after exclusion: ",n)
+
